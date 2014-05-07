49f7fb7f11fa9465bab41cc851b50933c5ce254d
cotrpepe <cotrpepe@gmail.com>
2014-05-07 19:13:35 +0900

    Generated LinkedList class.

--- c++/linked_list.h
+++ c++/linked_list.h
@@ -5,17 +5,61 @@
 
 using namespace std;
 
-class LinkedList {
+template <typename T> class LinkedList {
 public:
-	LinkedList(void);
-	~LinkedList(void);
-	void print(void);
-	void add(int value);
-	bool del(int value);
+	LinkedList(void) : head(NULL), last(NULL) {}
+
+	~LinkedList(void) {
+		for (Node *n = head; n != NULL;) {
+			Node *remove_node = n;
+			n = n->next;
+			delete remove_node;
+		}
+	}
+
+	void print(void) {
+		for (Node *n = head; n != NULL; n = n->next) {
+			cout << n->value;
+			if (n->next != NULL) cout << " ";
+		}
+		cout << endl;
+	}
+
+	void add(T value) {
+		Node *n = new Node;
+		n->value = value;
+		n->next = NULL;
+		if (head == NULL) {
+			head = n;
+			last = n;
+		} else {
+			last->next = n;
+			last = n;
+		}	
+	}
+
+	bool del(T value) {
+		bool found = false;
+		Node *current = head;
+		Node *prev = NULL;
+		while (current != NULL) {
+			if (current->value == value) {
+				found = true;
+				if (prev == NULL) {
+					head = current->next;
+				} else {
+					prev->next = current->next;
+				}
+			}
+			prev = current;
+			current = current->next;
+		}
+		return found;
+	}
 
 private:
 	struct Node {
-		int value;
+		T value;
 		Node *next;
 	};
 
--- c++/main.cpp
+++ c++/main.cpp
@@ -1,19 +1,44 @@
 #include "linked_list.h"
+#include <string>
 
 int main(void) {
-	LinkedList list;
+	LinkedList<int> list;
+
 	list.add(1);
 	list.add(2);
 	list.add(3);
-	list.add(5);
+	list.add(4);
 	list.print();
 
 	cout << list.del(1) << endl;
 	list.print();
 
+	cout << list.del(9) << endl;
+	list.print();
+
 	cout << list.del(3) << endl;
 	list.print();
 
-	cout << list.del(5) << endl;
+	cout << list.del(4) << endl;
 	list.print();
+
+	LinkedList<string> list2;
+
+	list2.add("aaa");
+	list2.add("abb");
+	list2.add("bbb");
+	list2.add("ccc");
+	list2.print();
+
+	cout << list2.del("aaa") << endl;
+	list2.print();
+
+	cout << list2.del("xxx") << endl;
+	list2.print();
+
+	cout << list2.del("bbb") << endl;
+	list2.print();
+
+	cout << list2.del("ccc") << endl;
+	list2.print();
 }
