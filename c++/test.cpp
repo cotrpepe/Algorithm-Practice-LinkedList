2a8dc2f33af380c1cf16516ac259538cb12d90e9
cotrpepe <cotrpepe@gmail.com>
2014-05-10 00:37:38 +0000

    Modify for Google Test

--- c++/test.cpp
+++ c++/test.cpp
@@ -2,17 +2,21 @@
 #include <string>
 #include "linked_list.h"
 
-TEST(LinkedList, print) {
+TEST(LinkedList, add_and_del) {
         LinkedList<int> list;
         list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        //ASSERT_EQ("1 2 3 4", list.print());
+	list.add(2);
+	list.add(3);
+
+	ASSERT_FALSE(list.del(0));
+	ASSERT_TRUE(list.del(1));
+	ASSERT_TRUE(list.del(3));
+	ASSERT_TRUE(list.del(2));
+	ASSERT_FALSE(list.del(2));
 }
-/*
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
-*/
+
